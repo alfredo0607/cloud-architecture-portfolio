84e141271ebc2ddec445662cf70bbb7399e9dbb1
Alfredo0607 <alfredojosedominguezhernandez@gmail.com>
2026-05-17 23:39:24 -0500

    feat: Add Terraform configuration for private CDN setup
    - Created terraform.tfstate for AWS CloudFront distribution, key group, origin access control, public key, KMS key, S3 bucket, and associated policies.
    - Added terraform.tfvars to define environment variable.
    - Included LICENSE.txt for the provider.
    - Added AWS provider executable for Windows.
    - Created a backup of the previous terraform.tfstate.

---
+++ infrastructure/terraform/01-private-cdn/main.tf
@@ -0,0 +1,203 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.45.0"
+    }
+  }
+
+  required_version = ">= 1.4.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "leader-developer-personal"
+}
+
+#################################################
+# VARIABLES
+#################################################
+
+variable "env" {
+  type = string
+}
+
+
+#################################################
+# KMS KEY
+#################################################
+
+resource "aws_kms_key" "s3" {
+  description         = "KMS key for S3 encryption"
+  enable_key_rotation = true
+}
+
+#################################################
+# S3 BUCKET
+#################################################
+
+resource "aws_s3_bucket" "assets" {
+  bucket = "my-private-assets-${var.env}-2026-demo"
+}
+
+#################################################
+# BLOCK PUBLIC ACCESS
+#################################################
+
+resource "aws_s3_bucket_public_access_block" "assets" {
+  bucket = aws_s3_bucket.assets.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
+
+#################################################
+# SERVER SIDE ENCRYPTION
+#################################################
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "assets" {
+  bucket = aws_s3_bucket.assets.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = aws_kms_key.s3.arn
+    }
+
+    bucket_key_enabled = true
+  }
+}
+
+#################################################
+# CLOUDFRONT PUBLIC KEY
+#################################################
+
+resource "aws_cloudfront_public_key" "main" {
+  name        = "cdn-public-key"
+  comment     = "Public key for signed URLs"
+  encoded_key = file("${path.module}/public_key.pem")
+}
+
+#################################################
+# KEY GROUP
+#################################################
+
+resource "aws_cloudfront_key_group" "signed_urls" {
+  name  = "signed-url-key-group"
+  items = [aws_cloudfront_public_key.main.id]
+}
+
+#################################################
+# ORIGIN ACCESS CONTROL
+#################################################
+
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "s3-oac"
+  description                       = "OAC for private S3 bucket"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+#################################################
+# CLOUDFRONT DISTRIBUTION
+#################################################
+
+resource "aws_cloudfront_distribution" "cdn" {
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+  price_class         = "PriceClass_100"
+
+  origin {
+    domain_name              = aws_s3_bucket.assets.bucket_regional_domain_name
+    origin_id                = "s3-private-assets"
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "s3-private-assets"
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = [
+      "GET",
+      "HEAD"
+    ]
+
+    cached_methods = [
+      "GET",
+      "HEAD"
+    ]
+
+    trusted_key_groups = [
+      aws_cloudfront_key_group.signed_urls.id
+    ]
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl     = 0
+    default_ttl = 86400
+    max_ttl     = 604800
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+#################################################
+# BUCKET POLICY
+#################################################
+
+resource "aws_s3_bucket_policy" "assets" {
+
+  depends_on = [
+    aws_cloudfront_distribution.cdn
+  ]
+
+  bucket = aws_s3_bucket.assets.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Sid    = "AllowCloudFrontAccess"
+        Effect = "Allow"
+
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+
+        Action = [
+          "s3:GetObject"
+        ]
+
+        Resource = [
+          "${aws_s3_bucket.assets.arn}/*"
+        ]
+
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.cdn.arn
+          }
+        }
+      }
+    ]
+  })
+}
